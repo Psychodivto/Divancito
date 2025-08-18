48950002386b896cf1f98a94a32ffa3ba849cd93
psychodivto <perezcondediegoivan@outlook.com>
2025-08-18 14:10:13 -0600

    reajusts GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,15 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://psychodivto.github.io",
-  base: "/my-repo/",
-  integrations: [
-    tailwind(),
-    icon(),
-    remarkReadingTime(),
-    node(),
-  ],
+  base: "/", // 👈 cámbialo a "/" si tu repo se llama psychodivto.github.io
+
+  integrations: [tailwind(), icon()],
+  output: "static",
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
 });
